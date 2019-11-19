83681857026fed9ccaf4f3fee97e6b29c58c2a0f
Ozgen Tastan <efaithm@windowslive.com>
2019-11-18 17:22:52 -0800

    Controllers done

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,16 @@ const ObjectId = require('mongodb').ObjectId;
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 const authentication = require('../authentication');
+// User Controller for user related requests
 
+/** 
+ * @param username
+ * Pass the username in request body as username
+ * @param password
+ * Pass the password in request body as password
+ * 
+ * Returns auth token if successfull, returns error if not
+*/
 module.exports.userLogin = async (req, res, next) => {
     try {
     const userCollection = req.app.db.collection('Users');
@@ -17,7 +26,6 @@ module.exports.userLogin = async (req, res, next) => {
     }
 
     // Check if hashed passwords match
- 
         const hash = crypto.createHash('sha256');
         hash.update(req.body.password);
 
@@ -41,6 +49,14 @@ module.exports.userLogin = async (req, res, next) => {
     }
 }
 
+/**
+ * @param username
+ * Pass the username in request body as username
+ * @param password
+ * Pass the password in request body as password
+ * 
+ * Returns success message or error depending on the result
+ */
 module.exports.createUser = async(req, res, next) => {
     try {
     const userCollection = req.app.db.collection('Users');
@@ -67,6 +83,42 @@ module.exports.createUser = async(req, res, next) => {
     } catch (err) {
         return next(err);
     }
+}
 
+module.exports.changePassword = async(req, res, next) => {
+    if (!authentication.authenticate(req, res))
+        return;
+    try {
+        let newPassword, currentPassword, enteredCurrentPassword;
+        const hash = crypto.createHash('sha256');
 
+        enteredCurrentPassword = req.body.enteredCurrentPassword;
+
+        let user = await req.app.db.collection('Users').findOne({username: req.body.username});
+        if(!user) {
+            return next(new Error('This user does not exist'));
+        }
+        currentPassword = user.password;
+        hash.update(enteredCurrentPassword);
+        if (currentPassword != hash.digest('base64')) {
+            return next(new Error('Password is incorrect'));
+        } else {
+            newPassword = req.body.newPassword;
+            const hashNewPassword = crypto.createHash('sha256');
+            hashNewPassword.update(newPassword);
+            const digestedNewPassword = hashNewPassword.digest('base64');
+            if (currentPassword == digestedNewPassword) {
+                return next(new Error('New password cannot be the same as the old one'));
+            }
+
+            let response = await req.app.db.collection('Users').updateOne(
+                {username: req.body.username},
+                {$set: {password: digestedNewPassword}}
+            )
+            
+            res.send("Password successfully changed");
+        }
+    } catch (err) {
+        return next(err);
+    }
 }
