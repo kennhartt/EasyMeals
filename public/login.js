18cfc47d78f9ff998dce195d7fadd7140e0bae96
Kenny <kennyhsunarya@gmail.com>
2019-11-18 18:55:19 -0800

    Login added

---
+++ public/login.js
@@ -0,0 +1,17 @@
+function processLogin(username,password){
+	$.ajax({
+		url: 'http://localhost:8000/api/user/login',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({username,password}),
+		success: function(res){
+			console.log(res);
+			localStorage.setItem("userInfo",JSON.stringify(res));
+			window.location.href = "Registration_success.html";
+		},
+		error: function(res){
+			console.log("Fuck off");
+		}
+		
+	})	
+}
