84ef4c95f99fb990fba9e237d1325464d925c0b5
Ozgen Tastan <efaithm@windowslive.com>
2019-11-19 22:44:34 -0800

    please ne done

--- authentication.js
+++ authentication.js
@@ -3,13 +3,17 @@ const config = require('./config');
 
 //Authentication function for keeping user sessions
 module.exports.authenticate = (req,res) => {
+  //Checks if the token is there
     const token = req.headers['x-access-token'];
     let authFailed = false;
+    // If token is not there, means user is not logged in
     if (!token) 
     {
         res.status(401).send({ auth: false, message: 'No token provided.' });
         return false;
     }
+
+    //If token is there, verify it with secret code to see if it is correct
     jwt.verify(token, config.secret, function(err, decoded) {
         if (err) 
         {
