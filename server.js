d2a0faae31842229970b4248e63b733b27972dca
Ozgen Tastan <efaithm@windowslive.com>
2019-11-25 12:35:18 -0800

    Cors

--- server.js
+++ server.js
@@ -18,6 +18,22 @@ client.connect(function(err, client) {
     }
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 // Node configuration
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
