0c4fab7177485491a343f0c7ee57b9dd46f2361f
Kenny <kennyhsunarya@gmail.com>
2020-04-08 19:40:29 -0700

    undo

--- EasyMealsWeb/pages/index.js
+++ EasyMealsWeb/pages/index.js
@@ -1,19 +1,19 @@
 import fetch from "node-fetch";
 import Link from "next/link";
 
-const RecipeLink = props => (
+const RecipeLink = (props) => (
   <li>
-    <Link href='/recipe/[recipeId]' as={`/recipe/${props.recipeId}`}>
+    <Link href="/recipe/[recipeId]" as={`/recipe/${props.recipeId}`}>
       <a>{props.title}</a>
     </Link>
   </li>
-)
+);
 
 export default function Index() {
   return (
     <ul>
-      <RecipeLink title = 'Breakfast Pizza' recipeId='559251' />
-      <RecipeLink title = 'Egg and rocket pizzas' recipeId='630293' />
+      <RecipeLink title="Breakfast Pizza" recipeId="559251" />
+      <RecipeLink title="Egg and rocket pizzas" recipeId="630293" />
     </ul>
   );
 }
